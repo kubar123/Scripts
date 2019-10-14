138ca850b9ba65452d37b4c5f226aa25f414b91e
Kubar123 <kubar123@hotmail.com>
2019-10-14 12:46:20 +1100

    Version modified for auto update
    Version number changed

--- AirDatesLinks.user.js
+++ AirDatesLinks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AirDates Additional links
 // @namespace    https://github.com/kubar123/Scripts
-// @version      0.1
+// @version      0.1.1
 // @description  adds some additional links to airdates
 // @author       kubar123
 // @match        https://www.airdates.tv/
