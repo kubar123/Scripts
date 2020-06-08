bcf0f9d5451695feedc09a234cdaadc40af1d30f
Kubar123 <kubar123@hotmail.com>
2020-06-08 12:37:37 +1000

    updated version #
    updated version# for auto updates

--- IMDB/IMDBLinks.user.js
+++ IMDB/IMDBLinks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IMDB Additional Links
 // @namespace    https://github.com/kubar123/Scripts
-// @version      0.2.0
+// @version      0.3.0
 // @description  adds some additional links to airdates
 // @author       kubar123
 // @match        https://www.imdb.com/title/*
