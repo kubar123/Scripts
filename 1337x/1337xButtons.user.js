88c577f1d682c6d670de76800dc30dd37cb6274e
kubar123 <kubar123@users.noreply.github.com>
2025-02-10 07:48:02 +1100

    Code cleanup and refactoring

--- 1337x/1337xButtons.user.js
+++ 1337x/1337xButtons.user.js
@@ -9,85 +9,71 @@
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
+(() => {
     // Your code here...
-    var seasonNo, episodeNo;
-    var showStr;
+    let seasonNo;
+    let episodeNo;
+    let showStr;
 
     // ---- ADD DATA TO BOX ----
     grabDataFromSite();
     addDataToWindow();
     return;
 
-
-
-
-    function addDataToWindow(){
+    function addDataToWindow() {
         //Create buttons
-        // ------- EIPSODES ---------
-        var MainLinkDown=makeTvLink(showStr,parseInt(episodeNo)-1,seasonNo);
-        var buttonData="<button onclick='location.href=\"https://1337x.to/search/"+MainLinkDown+"/1/\"'>Previous</Button>__"
-        var MainLinkUp=makeTvLink(showStr,parseInt(episodeNo)+1,seasonNo);
-        buttonData+="<button onclick='location.href=\"https://1337x.to/search/"+MainLinkUp+"/1/\"'>Next</Button> | ~~ ";
+        // ------- EPISODES ---------
+        const MainLinkDown = makeTvLink(showStr, Number.parseInt(episodeNo)-1, seasonNo);
+        let buttonData = `<button onclick='location.href="https://1337x.to/search/${MainLinkDown}/1/"'>Previous</Button>__`;
+        const MainLinkUp = makeTvLink(showStr, Number.parseInt(episodeNo)+1, seasonNo);
+        buttonData += `<button onclick='location.href="https://1337x.to/search/${MainLinkUp}/1/"'>Next</Button> | ~~ `;
 
         // ---------- SEASONS ---------
-        var mainLinkDownS=makeTvLink(showStr,episodeNo,parseInt(seasonNo)-1);
-        buttonData+="|<button onclick='location.href=\"https://1337x.to/search/"+mainLinkDownS+"/1/\"'>Previous Season</Button> |";
-        var mainLinkUpS=makeTvLink(showStr,episodeNo,parseInt(seasonNo)+1);
-        buttonData+="<button onclick='location.href=\"https://1337x.to/search/"+mainLinkUpS+"/1/\"'>Next Season</Button>";
-
-
+        const mainLinkDownS = makeTvLink(showStr, episodeNo, Number.parseInt(seasonNo)-1);
+        buttonData += `|<button onclick='location.href="https://1337x.to/search/${mainLinkDownS}/1/"'>Previous Season</Button> |`;
+        const mainLinkUpS = makeTvLink(showStr, episodeNo, Number.parseInt(seasonNo)+1);
+        buttonData += `<button onclick='location.href="https://1337x.to/search/${mainLinkUpS}/1/"'>Next Season</Button>`;
 
         //grab the top box, append data to it
         $(".box-info").before().prepend(buttonData);
-
-
     }
 
-    function grabDataFromSite(){
-
+    function grabDataFromSite() {
         //get search from site
-        var searchTerm=$(".box-info-heading :contains('Searching for:')").text();
-        searchTerm=searchTerm.substr(2+searchTerm.indexOf(":"));
-        searchTerm=searchTerm.substr(0,searchTerm.indexOf(":")-3);
-
+        let searchTerm = $(".box-info-heading :contains('Searching for:')").text();
+        searchTerm = searchTerm.substr(2+searchTerm.indexOf(":"));
+        searchTerm = searchTerm.substr(0,searchTerm.indexOf(":")-3);
 
         //console.log(searchTerm);
 
         //find 'S**E**'
-        var reg=/S\d\dE\d\d/ig;
-        var seasonEpTerm= searchTerm.match(reg).toString();
+        const reg = /S\d\dE\d\d/ig;
+        const seasonEpTerm = searchTerm.match(reg).toString();
 
         //get the TV Show name -- remove matched string
-        showStr=searchTerm.replace(reg,"");
+        showStr = searchTerm.replace(reg,"");
         //console.log(showStr);
 
         //find season / episode numbers
         // -- [0] = season [1] = episode        
-        var reg2=/\d\d/ig;
-        var epSeasonInfo=seasonEpTerm.match(reg2);
-        seasonNo=epSeasonInfo[0];
-        episodeNo=epSeasonInfo[1];
+        const reg2 = /\d\d/ig;
+        const epSeasonInfo = seasonEpTerm.match(reg2);
+        seasonNo = epSeasonInfo[0];
+        episodeNo = epSeasonInfo[1];
 
         //console.log(epSeasonInfo.toString());
-        return
-
+        return;
     }
 
-    function makeTvLink(showN, epN, sN){
-
-        epN=parseInt(epN);
+    function makeTvLink(showN, epN, sN) {
+        let episodeNum = Number.parseInt(epN);
         //add '0' before ep name to keep format (S**E**)
-        if(epN<=9){
-            epN="0"+epN;
+        if(episodeNum <= 9) {
+            episodeNum = `0${episodeNum}`;
         }
 
         //make the main link
-        var mainLink=showN+" S"+sN+"E"+epN;
+        const mainLink = `${showN} S${sN}E${episodeNum}`;
         return mainLink;
     }
-
-
 })();
